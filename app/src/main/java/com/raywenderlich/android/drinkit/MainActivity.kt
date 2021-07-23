edc314f2422a688aef27583e8b3917b5a05ccfc0
Dhruv Limbachiya <dhruv.mail4work@gmail.com>
2021-07-23 11:22:30 +0530

    Set custom notification icon and color.

--- app/src/main/java/com/raywenderlich/android/drinkit/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/drinkit/MainActivity.kt
@@ -11,7 +11,6 @@ import com.google.firebase.iid.internal.FirebaseInstanceIdInternal
 import com.google.firebase.messaging.FirebaseMessaging
 import kotlinx.android.synthetic.main.activity_main.*
 
-// TODO: import libraries
 
 /**
  * Main Screen
